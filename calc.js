956e6a4115b87dee5c1d4961a721510950715a28
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-12-04 11:25:57 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,12 +1,8 @@
 function calc(callback){
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
-    var n3 = parseFloat(document.getElementById("n3").value);
-    var n4 = parseFloat(document.getElementById("n4").value);
     var result = callback(n1,n2);
-    var res = callback(n3,n4)
     document.getElementById("result").value = result;
-    document.getElementById("res").value = res;
 }
 function add(n1,n2){
     return n1 + n2;
@@ -20,8 +16,8 @@ function sub(n1,n2){
 function div(n1,n2){
     return n1 / n2;
 }
-function perc(n3,n4){
-    var c = n3 / n4;
+function perc(n1,n2){
+    var c = n1 / n2;
     var d = c * 100;
     return d;
 }
