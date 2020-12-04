3f59e11864fd2f568bdac32f89993b6506270de6
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-12-04 10:25:21 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,18 @@
+function calc(callback){
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
+    var result = callback(n1,n2);
+    document.getElementById("result").value = result;
+}
+function add(n1,n2){
+    return n1 + n2;
+}
+function mul(n1,n2){
+    return n1 * n2;
+}
+function sub(n1,n2){
+    return n1 - n2;
+}
+function div(n1,n2){
+    return n1 / n2;
+}
